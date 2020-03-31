bac1e77cc4706b31c17f1e08c4047a2a9dcf5145
WildPengu <7241679@wp.pl>
2020-03-31 12:02:04 +0200

    Updates

---
+++ precache-manifest.d83bd4decc4e1d34ca54aaa313eb1414.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29acd538cc2a01d7cc641325484888a9",
+    "url": "/currency-app/index.html"
+  },
+  {
+    "revision": "f1a6a9f637ff96f3bbd2",
+    "url": "/currency-app/static/css/main.9184d133.chunk.css"
+  },
+  {
+    "revision": "a849096d339f218c17ff",
+    "url": "/currency-app/static/js/2.76617f8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-app/static/js/2.76617f8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a6a9f637ff96f3bbd2",
+    "url": "/currency-app/static/js/main.561a1e64.chunk.js"
+  },
+  {
+    "revision": "dcbd6a995d5feb33f8fc",
+    "url": "/currency-app/static/js/runtime-main.9e4e49fb.js"
+  },
+  {
+    "revision": "f528b6497a33bb6f79e2f681de6dfe42",
+    "url": "/currency-app/static/media/background.f528b649.jpg"
+  }
+]);
